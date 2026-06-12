8e56b90249a1723178d25989ec1fa981cf4e9f93
AleksaNik25 <sashanikiforova2005@gmail.com>
2026-06-12 13:49:12 +0300

    db

--- config/db.php
+++ config/db.php
@@ -1,5 +1,19 @@
 <?php
 
+if (str_contains($_SERVER["SERVER_NAME"], "beget")) {
+    return [
+        'class' => 'yii\db\Connection',
+        'dsn' => 'mysql:host=localhost;dbname=sashanlg_market',
+        'username' => 'sashanlg_market',
+        'password' => 'SashanlgMarket_',
+        'charset' => 'utf8',
+
+        // Schema cache options (for production environment)
+        'enableSchemaCache' => true,
+        'schemaCacheDuration' => 60,
+        'schemaCache' => 'cache',
+    ];
+}
 
 if (str_contains($_SERVER["SERVER_NAME"], "prof.ru")) {
     return [
